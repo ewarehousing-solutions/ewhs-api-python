5ae9d010b87dfde3f633887cea30d53411832e27
Mark Everaert <mark.everaert@notive.nl>
2023-05-01 16:00:18 +0200

    Added webhook and products to the core endpoints

---
+++ tests/test_webhooks.py
@@ -0,0 +1,14 @@
+def test_list_webhooks(authenticated_client, response):
+    """Retrieve a list of webhooks"""
+    response.get(f"https://api.ewarehousing.com/webhooks/", "webhook_list")
+
+    webhooks = authenticated_client.webhook.list()
+    assert isinstance(webhooks, dict)
+    assert webhooks['results'] != None
+    #
+    assert isinstance(webhooks['results'], list)
+    assert len(webhooks['results']) == 2
+    #
+    assert webhooks['results'][0]['group'] == 'stock'
+    assert webhooks['results'][0]['action'] == 'updated'
+
